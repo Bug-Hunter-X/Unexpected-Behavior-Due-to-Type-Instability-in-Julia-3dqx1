5c725bc99f02d9723eb295f1947d9bb512a1a699
BugHunter-X <askzenai@gmail.com>
2024-12-30 13:00:07 +0100

    Add bug.jl file

---
+++ bug.jl
@@ -0,0 +1,39 @@
+```julia
+function my_function(x)
+  if x > 0
+    return x^2
+  else
+    return 0
+  end
+end
+
+result = my_function(-1)
+println(result) # Output: 0
+
+result = my_function(2)
+println(result) # Output: 4
+
+result = my_function(0)
+println(result) #Output: 0
+
+#This is all fine, but unexpected behavior happens when we slightly change this code
+function my_function(x::Int64)
+  if x > 0
+    return x^2
+  else
+    return 0
+  end
+end
+
+result = my_function(-1)
+println(result) # Output: 0
+
+result = my_function(2)
+println(result) # Output: 4
+
+result = my_function(0)
+println(result) #Output: 0
+
+result = my_function(2.5) #this line will cause an error
+println(result)
+```
